7ec9fe4a17306894d5ea9dccd3693735c6f82814
MatysApiros <matheus.canabarro@hotmail.com>
2017-10-23 11:26:45 -0200

    Piu!

--- src/main/java/br/com/matysapiros/bookchooser/EnumGenre.java
+++ src/main/java/br/com/matysapiros/bookchooser/EnumGenre.java
@@ -1,4 +1,32 @@
 package br.com.matysapiros.bookchooser;
 
 public enum EnumGenre {
+    COMEDIA("Comédia"),
+    TERROR("Terror"),
+    FICCAO("Ficção"),
+    FARSA("Farsa"),
+    MELODRAMA("Melodrama"),
+    PANTOMIMA("Pantomima"),
+    PERFORMANCE("Performance"),
+    TEATRO("Teatro"),
+    TRAGEDIA("Tragédia"),
+    TRAGICOMEDIA("Tragicomédia"),
+    EPOPEIA("Epopéia"),
+    CONTO("Conto"),
+    NOVELA("Novela"),
+    ROMANCE("Romance"),
+    CRONICA("Crônica"),
+    FABULA("Fabula"),
+    MISTERIO("Mistério"),
+    ENSAIO("Ensaio"),
+    SATIRA("Sátira");
+    private String genre;
+
+    EnumGenre(String genre){
+        this.genre = genre;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
 }
