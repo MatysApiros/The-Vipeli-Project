f8fab61166e605cd1c701cfa038873a7209563b7
MatysApiros <matheus.canabarro@hotmail.com>
2017-10-26 10:50:27 -0200

    Inserção consulta no banco implementadas(Class Insertion). Testes feitos e passando.

--- src/main/java/br/com/matysapiros/bookchooser/dados/Insertion.java
+++ src/main/java/br/com/matysapiros/bookchooser/dados/Insertion.java
@@ -1,6 +1,7 @@
 package br.com.matysapiros.bookchooser.dados;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Insertion {
@@ -21,8 +22,26 @@ public class Insertion {
             statementmt.execute();
             statementmt.close();
             return true;
-        }catch (SQLException u){
-            throw new RuntimeException(u);
+        }catch (SQLException excption){
+            throw new RuntimeException(excption);
+        }
+    }
+
+    public boolean selectBook(String nome){
+        String sql = "select * from books where title like ?";
+        try{
+            PreparedStatement statementmt = connection.prepareStatement(sql);
+            statementmt.setString(1, nome);
+            ResultSet string = statementmt.executeQuery();
+            while (string.next()) {
+                String title = string.getString("title");
+                System.out.println("Livro: " + title);
+            }
+            statementmt.execute();
+            statementmt.close();
+            return true;
+        } catch (SQLException excption){
+            throw new RuntimeException(excption);
         }
     }
 }
--- src/test/java/br/com/matysapiros/bookchooser/dados/InsertionTest.java
+++ src/test/java/br/com/matysapiros/bookchooser/dados/InsertionTest.java
@@ -6,6 +6,12 @@ import static org.junit.Assert.*;
 
 public class InsertionTest {
     @Test
+    public void selectBook() throws Exception {
+        Insertion insertion = new Insertion();
+        org.junit.Assert.assertTrue(insertion.selectBook("Eragon"));
+    }
+
+    @Test
     public void insertBook() throws Exception {
         Insertion insertion = new Insertion();
         org.junit.Assert.assertTrue(insertion.insertBook("Eragon",1,8.75,1));
