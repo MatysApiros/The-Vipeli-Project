4f968f1e9308503404aaa858fb4ba71a81d3c0ea
Matheus Canabarro <matheus.canabarro@hotmail.com>
2017-11-06 09:50:39 -0200

    Update Tables.sql

--- Tables.sql
+++ Tables.sql
@@ -1,25 +1,24 @@
 create table books(
-	book_id int(11) not null,
+	book_id int(11) auto_increment,
 	title varchar(100) not null,	
     numeracao decimal(10,0) not null,
     avaliacao numeric(4,2),
     read_flag decimal(10,0),
     type_id int(10),
     
-    constraint pk_books primary key(book_id),
-    constraint fk_books_types foreign key (type_id) references books_types(type_id)
+    constraint pk_books primary key(book_id)
 );
 
 create table genres(
-	genre_id int(10),
+	genre_id int(10) auto_increment,
     genre varchar(50),
     
     constraint pk_genres primary key(genre_id)
 );
 
 create table books_types(
-	type_id int(10),
-    description varchar(10),
+	type_id int(10) auto_increment,
+    description varchar(50),
     
     constraint pk_books_types primary key(type_id)
 );
@@ -30,3 +29,5 @@ create table books_and_genres(
     
     constraint pk_books_and_genres primary key(book_id,genre_id)
 );
+
+alter table books add constraint fk_books_types foreign key (type_id) references books_types(type_id);
